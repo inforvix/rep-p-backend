99fe78318d157c8ea885db2d6abfdb210edd41b5
Eduardo Guedes <eduardoguedesdev@gmail.com>
2025-04-30 10:36:06 -0300

    parametros para saber se a empresa solicita senha funcionario

--- helpers/create-token-funcionario.js
+++ helpers/create-token-funcionario.js
@@ -1,4 +1,5 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const Empresa = require('../models/Empresa');
 
 const createUserTokenFuncionario = async(funcionario,req,res)=> {
   const token = jwt.sign({
@@ -7,13 +8,15 @@ const createUserTokenFuncionario = async(funcionario,req,res)=> {
     fun_rep_padrao: funcionario.rep_padrao
   }, "ASc3hTKprW#4gH06S746&HN%#")
 
-  //console.log(fun_cpf)
-  console.log(funcionario.EmpresaId)
+  const empresa = await Empresa.findOne({
+    where: { id: funcionario.EmpresaId },
+  });
 
   res.status(200).json({
     message:"Você está autenticado",
     token: token,
-    empresa:funcionario.EmpresaId
+    empresa:funcionario.EmpresaId,
+    solicitaFoto: empresa.solicita_foto_registrar_marcacao,
   })
 
 }
